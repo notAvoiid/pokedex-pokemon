0a0e0b5a4fd329a44de6b02907bcf154be0c91f3
Igor Nascimento <igor.nacimento51@gmail.com>
2024-05-08 14:31:50 -0300

    feat: refactoring pokémon dto

--- src/main/java/com/crud/pokemon/model/dto/pokemon/PokemonRequestDTO.java
+++ src/main/java/com/crud/pokemon/model/dto/pokemon/PokemonRequestDTO.java
@@ -2,35 +2,35 @@ package com.crud.pokemon.model.dto.pokemon;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class PokemonRequestDTO {
+public record PokemonRequestDTO (
 
     @NotBlank(message = "Name must not be blank!")
     @Size(min = 4, max = 30, message = "Name must be between 4 and 20 characters")
-    private String name;
+    String name,
 
     @NotBlank(message = "Category must not be blank!")
     @Size(min = 4, max = 30, message = "Category must be between 4 and 20 characters")
-    private String category;
+    String category,
 
     @NotBlank(message = "Abilities must not be blank!")
     @Size(min = 4, max = 30, message = "Abilities must be between 4 and 20 characters")
-    private String abilities;
+    String abilities,
 
     @NotBlank(message = "Type must not be blank!")
     @Size(min = 4, max = 30, message = "Type must be between 4 and 20 characters")
-    private String type;
-    private String weakness;
+    String type,
 
-    private String height;
-    private String weight;
+    @NotBlank(message = "Weakness must not be blank!")
+    @Size(min = 4, max = 30, message = "Weakness must be between 4 and 20 characters")
+    String weakness,
 
-}
+    @NotBlank(message = "Height must not be blank!")
+    @Size(min = 4, max = 30, message = "Height must be between 4 and 20 characters")
+    String height,
+
+    @NotBlank(message = "Weight must not be blank!")
+    @Size(min = 4, max = 30, message = "Weight must be between 4 and 20 characters")
+    String weight
+
+) {}
--- src/main/java/com/crud/pokemon/model/dto/pokemon/PokemonResponseDTO.java
+++ src/main/java/com/crud/pokemon/model/dto/pokemon/PokemonResponseDTO.java
@@ -1,30 +1,11 @@
 package com.crud.pokemon.model.dto.pokemon;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class PokemonResponseDTO {
-
-    private Long id;
-    private String name;
-    private String category;
-    private String abilities;
-
-    private String type;
-    private String weakness;
-
-    private String height;
-    private String weight;
-
-
-    public PokemonResponseDTO(PokemonRequestDTO pokemon) {
-        this.name = pokemon.getName();
-        this.abilities = pokemon.getAbilities();
-    }
-}
+public record PokemonResponseDTO (Long id,
+    String name,
+    String category,
+    String abilities,
+    String type,
+    String weakness,
+    String height,
+    String weight
+){}
